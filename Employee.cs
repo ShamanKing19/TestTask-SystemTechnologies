aec28950e6aef727c4958f8ffdc60ea027e90ba0
Георгий <gera.sukhomlin@gmail.com>
2021-06-02 01:54:31 +0200

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -17,18 +18,24 @@ namespace StaffClass
         public string Position { get; set; }
         public string Base_salary { get; set; }
         public string Login { get; set; }
-
         public string Premium
         {
             get
             {
-                return CountPremium(Hire_date, Position).ToString();
+                return CountPremium(Position).ToString();
             }
             set
             {
             }
         }
-
+        public double SubordinatesSummSalary
+        {
+            get
+            {
+                return getSummSalaryOfSubordinates();
+            }
+            set { }
+        }
 
         public Employee() { }
         public Employee(string Employee_id, string Name, string Hire_date, string Position, string Base_salary, string Login) 
@@ -39,37 +46,129 @@ namespace StaffClass
             this.Position = Position;
             this.Base_salary = Base_salary;
             this.Login = Login;
-            
         }
-        
-        public double CountPremium(string date, string position)
+
+        // Запрос к бд
+        private SQLiteDataReader dataTableQuerry(string dataBaseFileName, string querry)
         {
-            double premium;
-           
+            SQLiteConnection dbConnection;
+            SQLiteCommand sqlCommand = new SQLiteCommand();
+            dbConnection = new SQLiteConnection($"Data Source={dataBaseFileName};Version=3;");
+            dbConnection.Open();
+            sqlCommand.Connection = dbConnection;
+            sqlCommand.CommandText = querry;
+            SQLiteDataReader reader = sqlCommand.ExecuteReader();
+            return reader;
+        }
+
+
+        // Возвращает сумму зарплаты подчинённых
+        private double getSummSalaryOfSubordinates()
+        {
+            // Поиск id подчинённых
+            SQLiteDataReader reader = dataTableQuerry("base.db", $"SELECT Subordinate_id FROM Bosses WHERE Chief_id = {Employee_id};");
+            double summSalary = 0;
+
+            // Список с id подчинённых
+            List<object> subordinateIdList = new List<object>();
+            while (reader.Read())
+            {
+                subordinateIdList.Add(reader["Subordinate_id"]);
+            }
+
+            // Рассчёт суммарной зарплаты подчинённых
+            foreach (object id in subordinateIdList)
+            {
+                // Находим зарплату подчинённого
+                reader = dataTableQuerry("base.db", $"SELECT Base_salary FROM Staff WHERE Employee_id = {id};");
+                
+                // Находим суммарную зарплату
+                while (reader.Read())
+                {
+                    summSalary += Convert.ToDouble(reader["Base_salary"]);
+                }
+            }
+            
+            // НАДО К СУММАРНЫЙ ЗАРПЛАТЕ ДОБАВИТЬ ПРЕМИИ, А ОНИ ХРАНЯТСЯ НЕ В БАЗЕ ДАННЫХ, А РАССЧИТЫВАЮТСЯ В КОДЕ
+            return summSalary;
+        }
+
+        // Рассчёт опыта работы
+        private int getWorkExperience(string date)
+        {
             DateTime today = DateTime.Now;
             DateTime hire = Convert.ToDateTime(date);
 
             TimeSpan ts = today - hire;
             double days = (ts).TotalDays / 365;
+
             int years = (int)Math.Floor(days);
 
+            return years;
+        }
+        
+
+        
+        // Подсчёт премии сотрудника
+        private double CountPremium(string position)
+        {
+            double premium;
+            double salary = Convert.ToDouble(this.Base_salary);
+            int years = getWorkExperience(this.Hire_date);
+
+
+            // К премии manager'у и salesman'у надо добавить %суммарной зарплаты всех подчинённых
             switch (position)
             {
                 case "Employee":
-                    premium = Convert.ToDouble(this.Base_salary) * 0.03 * years;
+                    premium = years * 0.03;
+                    if (premium > 0.3)
+                    {
+                        premium = salary * 0.3;
+                    }
+                    else
+                    {
+                        premium *= salary;
+                    }
+
                     break;
+                
                 case "Manager":
-                    premium = Convert.ToDouble(this.Base_salary) * 0.05 * years;
+                    premium = years* 0.05;
+                    if (premium > 0.4)
+                    {
+                        premium = salary * 0.4;
+                    }
+                    else
+                    {
+                        premium *= salary;
+                    }
                     break;
+                
                 case "Salesman":
-                    premium = Convert.ToDouble(this.Base_salary) * 0.01 * years;
+                    premium = years * 0.01;
+                    if (premium > 0.35)
+                    {
+                        premium = salary * 0.35;
+                    }
+                    else
+                    {
+                        premium *= salary;
+                    }
                     break;
+
                 default:
                     premium = 0;
                     break;
             }
 
             return premium;
+        }
+
+        // Сюда поместить расчёт выработанных лет из CountPremium
+        private int CountWorkExperience() 
+        {
+            return 0;
         }
     }
 }
